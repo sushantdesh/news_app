0c449761255976ae10ee9fee8b4a1beb1809dcce
sushantdesh <riekmenrudolf@gmail.com>
2020-09-18 23:57:40 -0500

    Updates

---
+++ precache-manifest.c9856cb4a16ab85390d6f23d37fcc452.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "116858de91572b162e7b913dcf77c647",
+    "url": "/news_app/index.html"
+  },
+  {
+    "revision": "b5c87131ea144cd1624c",
+    "url": "/news_app/static/js/2.32bd4f21.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/news_app/static/js/2.32bd4f21.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3442dd2a22986e01a837",
+    "url": "/news_app/static/js/main.deb5031e.chunk.js"
+  },
+  {
+    "revision": "ee3cb7f0b86e67490bef",
+    "url": "/news_app/static/js/runtime-main.a4a62132.js"
+  }
+]);
