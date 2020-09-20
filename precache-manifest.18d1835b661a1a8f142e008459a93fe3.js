da1d3ba8c748afd37277f95ad40ad100a0268f5d
sushantdesh <riekmenrudolf@gmail.com>
2020-09-20 12:40:42 -0500

    Updates

--- precache-manifest.18d1835b661a1a8f142e008459a93fe3.js
+++ precache-manifest.18d1835b661a1a8f142e008459a93fe3.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "1182b43e8ece106ca6a3ae18b38a0f4d",
+    "revision": "bea9f932fc4e353741d262d36a875529",
     "url": "/news_app/index.html"
   },
   {
-    "revision": "a9c2844366b929aa0ab9",
+    "revision": "4ae5b5625d2d94f75814",
     "url": "/news_app/static/css/2.d9ad5f5c.chunk.css"
   },
   {
-    "revision": "a9c2844366b929aa0ab9",
-    "url": "/news_app/static/js/2.dcc02c94.chunk.js"
+    "revision": "4ae5b5625d2d94f75814",
+    "url": "/news_app/static/js/2.f7220fd4.chunk.js"
   },
   {
     "revision": "241e2720c0228744c712295a835ee806",
-    "url": "/news_app/static/js/2.dcc02c94.chunk.js.LICENSE.txt"
+    "url": "/news_app/static/js/2.f7220fd4.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "eae47f28853a60204b93",
-    "url": "/news_app/static/js/main.2fd996c2.chunk.js"
+    "revision": "d009b88ded21234b18dd",
+    "url": "/news_app/static/js/main.3f0ffaec.chunk.js"
   },
   {
     "revision": "ee3cb7f0b86e67490bef",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/news_app/precache-manifest.dacba65a9aff836ab9e3389243b8c5d7.js"
+  "/news_app/precache-manifest.18d1835b661a1a8f142e008459a93fe3.js"
 );
 
 self.addEventListener('message', (event) => {
